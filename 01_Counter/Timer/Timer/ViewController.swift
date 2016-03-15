8ce8f277828e475abd1e469c16bece2dce9955fe
John Peden <johnpeden@jped.local>
2016-03-15 13:35:53 -0500

    Changed names

--- 01_Counter/Timer/Timer/ViewController.swift
+++ 01_Counter/Timer/Timer/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonTItle: UIButton!
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTapped(sender: UIButton) {
+            counter++
+            let counterString = String(counter)
+            buttonTItle.setTitle(counterString, forState: UIControlState.Normal)
+    }
 
+    @IBAction func LongPress(sender: AnyObject) {
+        buttonTItle.setTitle("0", forState: UIControlState.Normal)
+        counter = 0
+    }
+    
 }
 
