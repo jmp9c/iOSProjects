c7c229146dda471db3beddecf229769d49cb9632
John Peden <johnpeden@jped.local>
2016-03-15 21:27:29 -0500

    Added the files to the dir

---
+++ 02_TipCalculator/TipCalculator/TipCalculator/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by John Peden on 3/15/16.
+//  Copyright © 2016 John Peden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var TipPercentageLabel: UILabel!
+    @IBOutlet weak var TipPercentageAmmount: UILabel!
+    @IBOutlet weak var TotalAmount: UILabel!
+    @IBOutlet weak var UserAmount: UITextField!
+    @IBOutlet weak var PercentageSlider: UISlider!
+    var PercentageAmount: Int {
+            return Int(100 * PercentageSlider.value)
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
+    }
+    
+    @IBAction func UserEditedPrice(sender: UITextField) {
+        /* Compute Tip Percentage Amount */
+        CalculateTipPercentageAmount()
+        CalculateTotalAmount()
+    }
+    
+    @IBAction func Slided(sender: UISlider) {
+        CalculateTipPercentageAmount()
+        CalculateTotalAmount()
+    }
+    
+    func CalculateTipPercentageAmount() {
+        
+        TipPercentageLabel.text = "\(PercentageAmount)%"
+        var uAmount = Float(UserAmount.text!)
+        if (uAmount == nil) {
+            uAmount = 0
+        }
+        let hAmount = uAmount! * Float(PercentageAmount)
+        let finalAmount = hAmount / 100
+        TipPercentageAmmount.text = String(format: "$%.2f", arguments: [finalAmount])
+    }
+    
+    func CalculateTotalAmount() {
+        var uAmount = Float(UserAmount.text!)
+        if (uAmount == nil) {
+            uAmount = 0
+        }
+        let hAmount = uAmount! * Float(PercentageAmount)
+        let finalAmount = hAmount / 100
+        
+        let total = finalAmount + uAmount!
+        TotalAmount.text = String(format: "$%.2f", arguments: [total])
+        
+    }
+    
+
+}
