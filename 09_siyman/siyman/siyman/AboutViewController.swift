90057e21133c6828167ecbc8c565aa71bbb889ed
John Peden <jmp9c@mtmail.mtsu.edu>
2016-04-02 20:39:00 -0500

    App is fully functional now

--- 09_siyman/siyman/siyman/AboutViewController.swift
+++ 09_siyman/siyman/siyman/AboutViewController.swift
@@ -1,38 +1,23 @@
 //
-//  HomeViewController.swift
+//  AboutViewController.swift
 //  siyman
 //
-//  Created by John Peden on 4/1/16.
+//  Created by John Peden on 4/2/16.
 //  Copyright © 2016 John Peden. All rights reserved.
 //
 
 import UIKit
 
-class HomeViewController: UIViewController {
-    
-    @IBOutlet weak var menuButton: UIBarButtonItem!
-    
-    @IBOutlet weak var backgroundImages: UIImageView!
-    
-    var images: [UIImage
-        ] = [UIImage(named: "ping-1.jpg")!, UIImage(named: "tmldr-14.jpg")!, UIImage(named: "DSC_3489.jpg")!, UIImage(named: "IMG_0808-1.JPG")!, UIImage(named: "IMG_0811-1.JPG")!, UIImage(named: "IMG_7977.JPG")!, UIImage(named: "MSSL-0564.jpg")!]
+class AboutViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
             menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-            
-            
-            
-            
         }
-        
-        self.backgroundImages.animationImages = images
-        self.backgroundImages.animationDuration = 17.0
-        self.backgroundImages.startAnimating()
-        
 
         // Do any additional setup after loading the view.
     }
@@ -41,8 +26,6 @@ class HomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
     
 
     /*
